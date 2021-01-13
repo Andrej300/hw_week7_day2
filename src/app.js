2efdf927615bfbb9645b4cfa90a9f50c9c946410
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-01-13 00:49:30 +0000

    methods_mounted

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import Vue from 'vue';
 
+Vue.filter('instructions', function(value){
+    value = "Click to convert currency";
+    return value;
+})
+
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el:"#app",
-        data: 56
-    
+        el: '#app',
+        data: {
+            rates: null,
+            base: "GBP",
+            selectedCurrency:"EUR",
+            amount: 0,
+        },
+        mounted: function () {
+            this.fetchRates()
+        },
+        methods: {        
+            fetchRates: function(){
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then( response => response.json() )
+                .then( data => this.rates = data.rates )
+            },
+        }
     });
 });
